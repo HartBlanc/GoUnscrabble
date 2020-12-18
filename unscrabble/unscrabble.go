855b2cdda5fa24b066a3ef6d4702ce3dbdcae097
Callum Hart <callumhartemail@gmail.com>
2020-12-18 00:25:27 +0000

    adds some cross check and board construction functionality

--- unscrabble/unscrabble.go
+++ unscrabble/unscrabble.go
@@ -1,30 +1,120 @@
 package unscrabble
 
-// Board is a data structure containing tiles.
-// It has methods for placing tiles on the board.
-type Board struct {
-	Tiles [][]*Tile
-}
+import "strings"
+
+var (
+	Empty        struct{}
+	letterScores = map[rune]int{
+		'a': 1,
+		'b': 4,
+		'c': 4,
+		'd': 2,
+		'e': 1,
+		'f': 4,
+		'g': 1,
+		'h': 3,
+		'i': 1,
+		'j': 10,
+		'k': 5,
+		'l': 2,
+		'm': 4,
+		'n': 2,
+		'o': 1,
+		'p': 4,
+		'q': 10,
+		'r': 1,
+		's': 1,
+		't': 1,
+		'u': 2,
+		'v': 5,
+		'w': 4,
+		'x': 8,
+		'y': 3,
+		'z': 10,
+	}
+	wwfLetterMultipliers = [][]int{
+		{3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 3, 1, 2, 1, 2, 1, 3, 1, 1},
+		{1, 1, 1, 3, 1, 1, 1, 3, 1, 1, 1},
+		{1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1},
+		{1, 1, 1, 3, 1, 1, 1, 3, 1, 1, 1},
+		{1, 1, 3, 1, 2, 1, 2, 1, 3, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3},
+	}
+	wwfWordMultipliers = [][]int{
+		{1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1},
+		{1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1},
+		{3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3},
+		{1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1},
+		{1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1},
+	}
+)
 
 // Tile is a data structure contains information relating to
 // the tile on the board.
 type Tile struct {
-	Letter rune
-	WordMultiplier int
+	Letter           rune
+	WordMultiplier   int
 	LetterMultiplier int
-	CrossChecks []bool
-	BoardPosition Position
+	CrossCheckSets   map[rune]struct{}
+	BoardPosition    *Position
 }
 
 type Position struct {
-	Row int
+	Row    int
 	Column int
+}
+
+type Lexicon interface {
+	Contains(string) bool
+	CrossSet(string, string) map[rune]struct{}
+}
+
+// NewTile returns a new empty tile, with a full cross check set.
+func NewTile(x, y, wordMultiplier, letterMultiplier int) *Tile {
+	return &Tile{
+		WordMultiplier:   wordMultiplier,
+		LetterMultiplier: letterMultiplier,
+		BoardPosition: &Position{
+			Row:    y,
+			Column: x,
+		},
+	}
+}
+
+// NewBoard returns a new empty board (a 2D slice of Tiles) from 2D slices
+// of word multipliers and letter multipliers.
+func NewBoard(wordMultipliers, letterMultipliers [][]int) [][]*Tile {
+	boardSize := len(wordMultipliers)
+	tiles := make([][]*Tile, boardSize)
+	for i := range tiles {
+		tiles[i] = make([]*Tile, boardSize)
+		for j := range tiles[i] {
+			tiles[i][j] = NewTile(
+				i,
+				j,
+				wordMultipliers[i][j],
+				letterMultipliers[i][j],
+			)
+		}
+	}
+	return tiles
 }
 
 // Transpose transposes the tiles of the board.
 // This is achieved using an in-place transformation.
 // This works on the assumption that the board is square.
-func Transpose(tiles [][]*Tile){
+func Transpose(tiles [][]*Tile) {
 	for i := range tiles {
 		for j := i + 1; j < len(tiles); j++ {
 			tiles[i][j], tiles[j][i] = tiles[j][i], tiles[i][j]
@@ -41,7 +131,7 @@ func GetAnchors(tiles [][]*Tile) []*Tile {
 	anchors := make([]*Tile, 0, len(tiles)*len(tiles))
 	for i, row := range tiles {
 		for j, tile := range row {
-			if !(tile.Letter == 0){
+			if !(tile.Letter == 0) {
 				continue
 			}
 
@@ -50,7 +140,7 @@ func GetAnchors(tiles [][]*Tile) []*Tile {
 				continue
 			}
 
-			if i < (len(tiles)-1) && tiles[i+1][j].Letter != 0{
+			if i < (len(tiles)-1) && tiles[i+1][j].Letter != 0 {
 				anchors = append(anchors, tile)
 				continue
 			}
@@ -60,7 +150,7 @@ func GetAnchors(tiles [][]*Tile) []*Tile {
 				continue
 			}
 
-			if j < (len(tiles)-1) && tiles[i][j+1].Letter != 0{
+			if j < (len(tiles)-1) && tiles[i][j+1].Letter != 0 {
 				anchors = append(anchors, tile)
 				continue
 			}
@@ -68,4 +158,73 @@ func GetAnchors(tiles [][]*Tile) []*Tile {
 
 	}
 	return anchors
+}
+
+// CrossCheck finds the cross check set and the cross score of a tile.
+// The cross check set of a tile is the set of letters
+// that will form legal down words when making an across
+// move through that square. The cross score is the sum of
+// the prefix score and the suffix score. Where the prefix score
+// and the suffix scores are the sums of the scores of the letters
+// of the prefix above the tile and the suffix below the tile.
+func CrossCheck(tile *Tile, tiles [][]*Tile, lexicon Lexicon) (map[rune]struct{}, int) {
+	prefix, prefixScore := GetPrefixAbove(tile, tiles)
+	suffix, suffixScore := GetSuffixBelow(tile, tiles)
+	if prefix == "" && suffix == "" {
+		return nil, 0
+	}
+	return lexicon.CrossSet(prefix, suffix), prefixScore + suffixScore
+}
+
+// GetPrefixAbove finds the prefix and the score
+// associated with the consecutive tiles immediately
+// above the provided tile.
+func GetPrefixAbove(tile *Tile, tiles [][]*Tile) (string, int) {
+
+	var sb strings.Builder
+	x := tile.BoardPosition.Column
+	y := tile.BoardPosition.Row - 1
+	score := 0
+
+	for ; (y >= 0) && tiles[y][x].Letter != 0; y-- {
+		placedTile := tiles[y][x]
+		sb.WriteRune(placedTile.Letter)
+		score += placedTile.LetterMultiplier * letterScores[placedTile.Letter]
+	}
+
+	return reverse(sb.String()), score
+}
+
+// GetSuffixBelow finds the suffix and the score
+// associated with the consecutive tiles immediately
+// below the provided tile.
+func GetSuffixBelow(tile *Tile, tiles [][]*Tile) (string, int) {
+
+	var sb strings.Builder
+	x := tile.BoardPosition.Column
+	y := tile.BoardPosition.Row + 1
+	score := 0
+
+	for ; (y < len(tiles)) && tiles[y][x].Letter != 0; y++ {
+		placedTile := tiles[y][x]
+		sb.WriteRune(placedTile.Letter)
+		score += placedTile.LetterMultiplier * letterScores[placedTile.Letter]
+	}
+
+	return sb.String(), score
+}
+
+// function, which takes a string as
+// argument and return the reverse of string.
+func reverse(s string) string {
+	rns := []rune(s) // convert to rune
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+
+		// swap the letters of the string,
+		// like first with last and so on.
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	// return the reversed string.
+	return string(rns)
 }
--- unscrabble/unscrabble_test.go
+++ unscrabble/unscrabble_test.go
@@ -1,19 +1,20 @@
 package unscrabble
 
 import (
-	"testing"
 	assert "github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func TestTranspose(t *testing.T){
+var testTiles = [][]*Tile{}
 
-	t.Run("transpose empty", func(t *testing.T){
+func TestTranspose(t *testing.T) {
+
+	t.Run("transpose empty", func(t *testing.T) {
 		tiles := [][]*Tile{{}}
 		Transpose(tiles)
 		assert.Equal(t, [][]*Tile{{}}, tiles)
 	})
-
-	t.Run("transpose single", func(t *testing.T){
+	t.Run("transpose single", func(t *testing.T) {
 		tiles := [][]*Tile{
 			{&Tile{}},
 		}
@@ -23,8 +24,7 @@ func TestTranspose(t *testing.T){
 		Transpose(tiles)
 		assert.Equal(t, expectedTiles, tiles)
 	})
-
-	t.Run("transpose square", func(t *testing.T){
+	t.Run("transpose square", func(t *testing.T) {
 		tiles := [][]*Tile{
 			{&Tile{Letter: 'a'}, &Tile{Letter: 'b'}},
 			{&Tile{Letter: 'c'}, &Tile{Letter: 'd'}},
@@ -45,14 +45,14 @@ func TestTranspose(t *testing.T){
 	})
 }
 
-func TestGetAnchors(t *testing.T){
+func TestGetAnchors(t *testing.T) {
 
-	t.Run("no tiles", func(t *testing.T){
+	t.Run("no tiles", func(t *testing.T) {
 		tiles := [][]*Tile{{}}
 		expectedAnchors := []*Tile{}
 		assert.Equal(t, expectedAnchors, GetAnchors(tiles))
 	})
-	t.Run("all tiles empty", func(t *testing.T){
+	t.Run("all tiles empty", func(t *testing.T) {
 		tiles := [][]*Tile{
 			{&Tile{}, &Tile{}},
 			{&Tile{}, &Tile{}},
@@ -60,7 +60,7 @@ func TestGetAnchors(t *testing.T){
 		expectedAnchors := []*Tile{}
 		assert.Equal(t, expectedAnchors, GetAnchors(tiles))
 	})
-	t.Run("no empty tiles", func(t *testing.T){
+	t.Run("no empty tiles", func(t *testing.T) {
 		tiles := [][]*Tile{
 			{&Tile{Letter: 'a'}, &Tile{Letter: 'a'}},
 			{&Tile{Letter: 'a'}, &Tile{Letter: 'a'}},
@@ -68,12 +68,12 @@ func TestGetAnchors(t *testing.T){
 		expectedAnchors := []*Tile{}
 		assert.Equal(t, expectedAnchors, GetAnchors(tiles))
 	})
-	t.Run("adjacent tiles are anchors", func(t *testing.T){
+	t.Run("adjacent tiles are anchors", func(t *testing.T) {
 
-		above := &Tile{BoardPosition: Position{Row:0, Column:1}}
-		left := &Tile{BoardPosition: Position{Row:1, Column:0}}
-		right := &Tile{BoardPosition: Position{Row:1, Column:2}}
-		below := &Tile{BoardPosition: Position{Row:2, Column:1}}
+		above := &Tile{BoardPosition: &Position{Row: 0, Column: 1}}
+		left := &Tile{BoardPosition: &Position{Row: 1, Column: 0}}
+		right := &Tile{BoardPosition: &Position{Row: 1, Column: 2}}
+		below := &Tile{BoardPosition: &Position{Row: 2, Column: 1}}
 		tiles := [][]*Tile{
 			{&Tile{}, above, &Tile{}},
 			{left, &Tile{Letter: 'a'}, right},
@@ -81,5 +81,96 @@ func TestGetAnchors(t *testing.T){
 		}
 		expectedAnchors := []*Tile{above, left, right, below}
 		assert.ElementsMatch(t, expectedAnchors, GetAnchors(tiles))
+	})
+}
+
+func TestGetPrefixAbove(t *testing.T) {
+	newBoard := func() [][]*Tile {
+		return [][]*Tile{
+			{NewTile(0, 0, 1, 1), NewTile(1, 0, 1, 1), NewTile(2, 0, 1, 1), NewTile(3, 0, 1, 1)},
+			{NewTile(0, 1, 1, 1), NewTile(1, 1, 1, 1), NewTile(2, 1, 1, 1), NewTile(3, 1, 1, 1)},
+			{NewTile(0, 2, 1, 1), NewTile(1, 2, 1, 1), NewTile(2, 2, 1, 1), NewTile(3, 2, 1, 1)},
+			{NewTile(0, 3, 1, 1), NewTile(1, 3, 1, 1), NewTile(2, 3, 1, 1), NewTile(3, 3, 1, 1)},
+		}
+	}
+	t.Run("test empty string and zero score if no prefix above", func(t *testing.T) {
+		tiles := newBoard()
+		prefix, score := GetPrefixAbove(tiles[1][1], tiles)
+		assert.Equal(t, "", prefix)
+		assert.Equal(t, 0, score)
+	})
+	t.Run("test does not go out of bounds if tile on top row", func(t *testing.T) {
+		tiles := newBoard()
+		prefix, score := GetPrefixAbove(tiles[0][1], tiles)
+		assert.Equal(t, "", prefix)
+		assert.Equal(t, 0, score)
+	})
+	t.Run("test does not go out of bounds if prefix stops at top", func(t *testing.T) {
+		tiles := newBoard()
+		tiles[0][1].Letter = 'a'
+		prefix, score := GetPrefixAbove(tiles[1][1], tiles)
+		assert.Equal(t, "a", prefix)
+		assert.Equal(t, 1, score)
+	})
+	t.Run("test stops at empty square", func(t *testing.T) {
+		tiles := newBoard()
+		tiles[0][1].Letter = 'b' // 1,1 is empty, should not reach here.
+		tiles[2][1].Letter = 'a'
+		prefix, score := GetPrefixAbove(tiles[3][1], tiles)
+		assert.Equal(t, "a", prefix)
+		assert.Equal(t, 1, score)
+	})
+	t.Run("prefix reverses correctly", func(t *testing.T) {
+		tiles := newBoard()
+		tiles[0][1].Letter = 'a'
+		tiles[1][1].Letter = 'b'
+		prefix, _ := GetPrefixAbove(tiles[2][1], tiles)
+		assert.Equal(t, "ab", prefix)
+	})
+}
+
+// TODO: here, and in prefix, check multipliers / score calculations.
+func TestGetSuffixBelow(t *testing.T) {
+	newBoard := func() [][]*Tile {
+		return [][]*Tile{
+			{NewTile(0, 0, 1, 1), NewTile(1, 0, 1, 1), NewTile(2, 0, 1, 1), NewTile(3, 0, 1, 1)},
+			{NewTile(0, 1, 1, 1), NewTile(1, 1, 1, 1), NewTile(2, 1, 1, 1), NewTile(3, 1, 1, 1)},
+			{NewTile(0, 2, 1, 1), NewTile(1, 2, 1, 1), NewTile(2, 2, 1, 1), NewTile(3, 2, 1, 1)},
+			{NewTile(0, 3, 1, 1), NewTile(1, 3, 1, 1), NewTile(2, 3, 1, 1), NewTile(3, 3, 1, 1)},
+		}
+	}
+	t.Run("test empty string and zero score if no suffix below", func(t *testing.T) {
+		tiles := newBoard()
+		suffix, score := GetSuffixBelow(tiles[1][1], tiles)
+		assert.Equal(t, "", suffix)
+		assert.Equal(t, 0, score)
+	})
+	t.Run("test does not go out of bounds if tile on bottom row", func(t *testing.T) {
+		tiles := newBoard()
+		suffix, score := GetSuffixBelow(tiles[len(tiles)-1][1], tiles)
+		assert.Equal(t, "", suffix)
+		assert.Equal(t, 0, score)
+	})
+	t.Run("test does not go out of bounds if suffix stops at bottom", func(t *testing.T) {
+		tiles := newBoard()
+		tiles[len(tiles)-1][1].Letter = 'a'
+		suffix, score := GetSuffixBelow(tiles[len(tiles)-2][1], tiles)
+		assert.Equal(t, "a", suffix)
+		assert.Equal(t, 1, score)
+	})
+	t.Run("test stops at empty square", func(t *testing.T) {
+		tiles := newBoard()
+		tiles[1][1].Letter = 'a'
+		tiles[3][1].Letter = 'b' // 2,1 is empty, should not reach here.
+		suffix, score := GetSuffixBelow(tiles[0][1], tiles)
+		assert.Equal(t, "a", suffix)
+		assert.Equal(t, 1, score)
+	})
+	t.Run("test suffix is not reversed", func(t *testing.T) {
+		tiles := newBoard()
+		tiles[1][1].Letter = 'a'
+		tiles[2][1].Letter = 'b'
+		suffix, _ := GetSuffixBelow(tiles[0][1], tiles)
+		assert.Equal(t, "ab", suffix)
 	})
 }

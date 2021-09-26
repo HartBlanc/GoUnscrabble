ed60018d1b6d8bee9b8d79e7376cbd99ff3bb336
Callum Hart <github@callumhart.me>
2021-09-26 14:04:20 +0100

    fix letterbag implementation

--- unscrabble/model/letterbag.go
+++ unscrabble/model/letterbag.go
@@ -17,33 +17,32 @@ func NewLetterBag(letterCounts map[rune]int) LetterBag {
 		numLetters += count
 	}
 	bag := make(LetterBag, 0, numLetters)
-	bag.AddLetterCounts(letterCounts)
+	bag.addLetterCounts(letterCounts)
 	return bag
 }
 
 // PopRandomLetter is for retrieving a random letter from the LetterBag
-func (bag LetterBag) PopRandomLetter() (rune, error) {
-	if len(bag) == 0 {
+func (bag *LetterBag) PopRandomLetter() (rune, error) {
+	if len(*bag) == 0 {
 		return 0, errors.New("bag is empty")
 	}
 
-	randomLetter := bag[len(bag)-1]
-	bag = bag[:len(bag)-1]
+	randomLetter := (*bag)[len(*bag)-1]
+	*bag = (*bag)[:len(*bag)-1]
 	return randomLetter, nil
 }
 
-// AddLetterCounts is for adding a batch of letters to the LetterBag
-func (bag LetterBag) AddLetterCounts(letterCounts map[rune]int) {
+func (bag *LetterBag) addLetterCounts(letterCounts map[rune]int) {
 	for letter, count := range letterCounts {
 		for i := 0; i < count; i++ {
-			bag = append(bag, letter)
+			*bag = append(*bag, letter)
 		}
 	}
 	bag.shuffle()
 }
 
-func (bag LetterBag) shuffle() {
-	rand.Shuffle(len(bag), func(i, j int) {
-		bag[i], bag[j] = bag[j], bag[i]
+func (bag *LetterBag) shuffle() {
+	rand.Shuffle(len(*bag), func(i, j int) {
+		(*bag)[i], (*bag)[j] = (*bag)[j], (*bag)[i]
 	})
 }
